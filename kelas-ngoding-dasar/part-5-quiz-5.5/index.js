61c22f5ab048ba08cd839061ce0db7336bf0a4d2
rezalastana <rezalastana21@gmail.com>
2023-11-27 14:44:32 +0700

    feat: quiz part 5.5

---
+++ kelas-ngoding-dasar/part-5-quiz-5.5/index.js
@@ -0,0 +1,119 @@
+//1. Let's Form a Sentence
+
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+//  Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+//  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+//   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+
+let word1 = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
+let seventh = "it!";
+
+//code here
+
+// ---------------------------------------
+console.log(
+    `${word1} ${second} ${third} ${fourth} ${fifth} ${sixth} ${seventh}`
+);
+console.log("=============================");
+// ---------------------------------------
+
+// 2. Index Accessing - 1 by 1
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+// Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+// Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word2 = "wow JavaScript is so cool";
+let exampleFirstWord = word2[0] + word2[1] + word2[2];
+let exampleSecondWord =
+    word2[4] +
+    word2[5] +
+    word2[6] +
+    word2[7] +
+    word2[8] +
+    word2[9] +
+    word2[10] +
+    word2[11] +
+    word2[12] +
+    word2[13] +
+    word2[14];
+let exampleThirdWord = word2[15] + word2[16];
+let exampleFourthWord = word2[18] + word2[19];
+let exampleFifthWord = word2[21] + word2[22] + word2[23] + word2[24];
+
+console.log("All word: " + word2);
+// ========================== //
+console.log("First Word: " + exampleFirstWord);
+
+// ---------------------------------------
+console.log("Second Word: " + exampleSecondWord);
+console.log("Third Word: " + exampleThirdWord);
+console.log("Fourth Word: " + exampleFourthWord);
+console.log("Fifth Word: " + exampleFifthWord);
+console.log("=============================");
+// ---------------------------------------
+
+// 3. Breaking Sentence (Again) using Substring
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+let exampleSecondWord3 = word3.substring(4, 14);
+let exampleThirdWord3 = word3.substring(15, 17);
+let exampleFourthWord3 = word3.substring(18, 20);
+let exampleFifthWord3 = word3.substring(21, 25);
+
+console.log("All word: " + word3);
+//  ------------------------------------ //
+console.log("First Word: " + exampleFirstWord3);
+
+// ---------------------------------------
+console.log("Second Word: " + exampleSecondWord3);
+console.log("Third Word: " + exampleThirdWord3);
+console.log("Fourth Word: " + exampleFourthWord3);
+console.log("Fifth Word: " + exampleFifthWord3);
+// ---------------------------------------
+
+// 4. Breaking Sentence (yet Again) and Count Each Length
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+let word4 = "wow JavaScript is so cool";
+let exampleFirstWord4 = word4.substring(0, 3);
+let exampleSecondWord4 = word4.substring(4, 14);
+let exampleThirdWord4 = word4.substring(15, 17);
+let exampleFourthWord4 = word4.substring(18, 20);
+let exampleFifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let secondWordLength = exampleSecondWord4.length;
+let thirdWordLength = exampleThirdWord4.length;
+let fourthWordLength = exampleFourthWord4.length;
+let fifthWordLength = exampleFifthWord4.length;
+
+console.log(
+    "First Word: " + exampleFirstWord + ", with length: " + firstWordLength
+);
+console.log(
+    "Second Word: " + exampleSecondWord4 + ", with length: " + secondWordLength
+);
+console.log(
+    "Third Word: " + exampleThirdWord4 + ", with length: " + thirdWordLength
+);
+console.log(
+    "Second Word: " + exampleFourthWord4 + ", with length: " + fourthWordLength
+);
+console.log(
+    "Second Word: " + exampleFifthWord4 + ", with length: " + fifthWordLength
+);
